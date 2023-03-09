0e9b5e96a7d1e9ba85d88fc37ea826867287922a
SVIS235 <touqeer.j@smart-v.com>
2023-03-09 12:10:49 +0500

    First Commit

---
+++ app/src/main/java/com/example/roomandroidkotlin/Subscriber.kt
@@ -0,0 +1,18 @@
+package com.example.roomandroidkotlin
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("subscriber_data_table")
+data class Subscriber (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "subscriber_id")
+    val id : Int ,
+
+    @ColumnInfo(name = "subscriber_name")
+    var name : String ,
+
+    @ColumnInfo(name = "subscriber_email")
+    var email: String
+    )
